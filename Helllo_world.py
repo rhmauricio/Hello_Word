02096d787310365b9d02f33cf1035a3d8669e15d
Orion-PC <bigbang.bigcrunsh@gmail.com>
2017-09-27 16:09:05 -0500

    Manejo de strings
    Algunos de los manejos que se les puede dar a los indices de los string

--- Helllo_world.py
+++ Helllo_world.py
@@ -1,1 +1,12 @@
-print('hello world')
+#Curso python primera semana
+
+#Manejor de strings y vectores en python 
+
+> str1 = 'hello'
+> str2 = ','
+> str3 = 'world'
+
+print(str1[-1]) 	# 'o'
+print('a' in str3) 	# True
+print(str3[:-1]) 	# 'worl'
+print(str3[::-1]) 	# 'dlrow'
