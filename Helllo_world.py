90904b1a5815b221b5ad541c5ca29db5ff62f02b
Mauricio Ramirez <mauricio.ramirez1@udea.edu.co>
2018-03-13 16:56:41 -0500

    prueba git

--- Helllo_world.py
+++ Helllo_world.py
@@ -5,8 +5,10 @@
 > str1 = 'hello'
 > str2 = ','
 > str3 = 'world'
+> str4 = 'holi'
 
 print(str1[-1]) 	# 'o'
 print('a' in str3) 	# True
 print(str3[:-1]) 	# 'worl'
 print(str3[::-1]) 	# 'dlrow'
+print(str4[::-1]) 	# 'dlrow'
